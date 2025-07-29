23cfd273699dde00207e6ff84680afe21ea6028f
NicolasMCascaes <cascaesnicolas7@gmail.com>
2025-07-29 12:36:33 -0300

    Better organization of the projetc Now working in the backend

---
+++ backend/src/main/java/com/project/calculia/CalculiaApplication.java
@@ -0,0 +1,13 @@
+package com.project.calculia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculiaApplication.class, args);
+	}
+
+}
