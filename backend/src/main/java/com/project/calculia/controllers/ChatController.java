79e5f13ffa1f1d239e2b92186bc1f4d87022dc0d
NicolasMCascaes <cascaesnicolas7@gmail.com>
2025-08-14 20:54:08 -0300

    backend is FINALY READY TO WORK

--- backend/src/main/java/com/project/calculia/controllers/ChatController.java
+++ backend/src/main/java/com/project/calculia/controllers/ChatController.java
@@ -1,5 +1,39 @@
 package com.project.calculia.controllers;
 
+import java.util.Map;
+
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.ollama.api.OllamaApi.ChatResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
 public class ChatController {
+
+    private final OllamaChatModel chatModel;
+
+    public ChatController(OllamaChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    @GetMapping("/ai/generate")
+    public Map<String, String> generate(
+            @RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", this.chatModel.call(message));
+    }
+
+    @PostMapping("/ai/prompt")
+    public Map<String, String> teste1(@RequestBody String entity) {
+
+        return Map.of("generation", this.chatModel.call(entity));
+    }
 
 }
