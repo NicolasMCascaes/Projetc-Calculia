7a2f8893ba7533497497560c7a15eb620b36ed95
NicolasMCascaes <cascaesnicolas7@gmail.com>
2025-08-14 21:10:37 -0300

    Trying to do clean code

--- backend/src/main/java/com/project/calculia/controllers/ChatController.java
+++ backend/src/main/java/com/project/calculia/controllers/ChatController.java
@@ -1,17 +1,8 @@
 package com.project.calculia.controllers;
 
 import java.util.Map;
-
-import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.ollama.OllamaChatModel;
-import org.springframework.ai.ollama.api.OllamaApi.ChatResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import reactor.core.publisher.Flux;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -24,16 +15,10 @@ public class ChatController {
         this.chatModel = chatModel;
     }
 
-    @GetMapping("/ai/generate")
-    public Map<String, String> generate(
-            @RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-        return Map.of("generation", this.chatModel.call(message));
-    }
-
     @PostMapping("/ai/prompt")
-    public Map<String, String> teste1(@RequestBody String entity) {
+    public Map<String, String> generateResponse(@RequestBody String request) {
 
-        return Map.of("generation", this.chatModel.call(entity));
+        return Map.of("generation", this.chatModel.call(request));
     }
 
 }
