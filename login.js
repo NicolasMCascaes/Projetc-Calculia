57a31c8c49cdc9a1830ea77d3f93a4e991625d28
NicolasMCascaes <cascaesnicolas7@gmail.com>
2025-08-17 14:15:30 -0300

    Change fetch link

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ document.getElementById("login-form").addEventListener("submit", function(event)
         username: document.getElementById("username").value,
         password: document.getElementById("password").value
     }
-    fetch("https://042c7a474392.ngrok-free.app/auth/login",{
+    fetch("https://ba2e73149bbd.ngrok-free.app/auth/login",{
         method:"POST",
         headers:{
             "Content-Type":"application/json",
