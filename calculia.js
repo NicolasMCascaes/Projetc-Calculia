ad269dbdbffdc8a51c73bdb003394ea495796f86
NicolasMCascaes <cascaesnicolas7@gmail.com>
2025-08-17 16:17:40 -0300

    Trying to make this work

--- calculia.js
+++ calculia.js
@@ -1,4 +1,4 @@
-document.getElementById("visor").addEventListener("submit", function(event){
+document.getElementById("explain-button").addEventListener("click", function(event){
     event.preventDefault();
     var visorValue = document.getElementById("visor").value
     const token = localStorage.getItem("token")
@@ -11,19 +11,19 @@ document.getElementById("visor").addEventListener("submit", function(event){
         },
         body:JSON.stringify({
             "message":"Você é um professor de matemática e precisa explicar a seguinte expressão:" + visorValue + "Você deve explicar de forma objetiva, clara, didática e sem nenhum erro"
-        })
-        .then(response =>{
+        }) 
+    })
+    .then(response =>{
             if(!response.ok){
                 throw new Error("Erro HTTP " + response.status)
             }
+            return response.ok
         })
         .then(data =>{
-            document.getElementById("responseArea").innerText = data.response
+            document.getElementById("responseArea").innerText = data.generation
         })
         .catch(error =>{
             console.error("Erro na requisição: ", error)
             document.getElementById("responseArea").innerText = "Não foi possível obter a resposta da IA. Aguarde alguns minutos e tente novamente!"
         })
-        
-    })
 })
