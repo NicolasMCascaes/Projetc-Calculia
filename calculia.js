2e7ec6a88d19e4f336daf39e0755552cc34d5020
NicolasMCascaes <cascaesnicolas7@gmail.com>
2025-08-19 12:58:25 -0300

    runnig

--- calculia.js
+++ calculia.js
@@ -3,7 +3,7 @@ document.getElementById("explain-button").addEventListener("click", function(eve
     var visorValue = document.getElementById("visor").value
     const token = localStorage.getItem("token")
     console.log(token)
-    fetch("https://87de6ad912ca.ngrok-free.app/ai/generate",{
+    fetch("https://87de6ad912ca.ngrok-free.app/ai/prompt",{
         method:"POST",
         headers:{
             "Content-Type":"application/json",
