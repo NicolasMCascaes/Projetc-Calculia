f23ce79bfb2c3176f1a3ba34ce8a96039657f1a3
NicolasMCascaes <cascaesnicolas7@gmail.com>
2025-08-21 21:48:40 -0300

    running

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ document.getElementById("login-form").addEventListener("submit", function(event)
         username: document.getElementById("username").value,
         password: document.getElementById("password").value
     }
-    fetch("https://87de6ad912ca.ngrok-free.app/auth/login",{
+    fetch("https://d256d17fc6fb.ngrok-free.app/auth/login",{
         method:"POST",
         headers:{
             "Content-Type":"application/json",
