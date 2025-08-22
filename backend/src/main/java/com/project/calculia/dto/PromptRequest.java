2494a5a20a7a9206e2c872504bb982d726129bf8
NicolasMCascaes <cascaesnicolas7@gmail.com>
2025-08-21 22:59:12 -0300

    Change the ChatController to make de AI generation work

---
+++ backend/src/main/java/com/project/calculia/dto/PromptRequest.java
@@ -0,0 +1,13 @@
+package com.project.calculia.dto;
+
+public class PromptRequest {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
